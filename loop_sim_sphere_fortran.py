58cd90fcfac819570aae4803af2338aa838993e4
epolimpio <epolimpio@gmail.com>
2015-08-06 10:30:18 +0200

    Loop for several parameters

---
+++ loop_sim_sphere_fortran.py
@@ -0,0 +1,51 @@
+import sim_sphere_fortran
+from myutils import readConfigFile
+from datetime import datetime
+
+# ----- PARAMETERS ----- #
+
+parameters = readConfigFile('parameters.ini')
+
+# simulation parameters
+n_steps = int(parameters['n_steps'])
+n_save = int(parameters['n_save'])
+ftype = int(parameters['ftype'])
+
+# timestep
+dt = float(parameters['dt'])
+
+# outfile for data
+tnow = datetime.now()
+outfile_video = datetime.strftime(tnow,str(parameters['outfile_video']))
+outfile_analysis = datetime.strftime(tnow,str(parameters['outfile_analysis']))
+outfile_postrotation = datetime.strftime(tnow,str(parameters['outfile_postrotation']))
+
+# ----- PARAMETERS TO LOOP ----- #
+# We write the parameters as tuples (N, phi_pack, nu_0, J, eta_n)
+
+all_N = [50, 100, 200]
+all_phi = [1]
+all_nu = [0.25, 0.5, 1, 2]
+all_J = [0.25, 0.5, 1, 2]
+all_eta = [0.25, 0.5, 1, 2]
+
+combinations = []
+for N in all_N:
+    for phi in all_phi:
+        for nu in all_nu:
+            for J in all_J:
+                for eta in all_eta:
+                    combinations.append((N,phi,nu,J,eta))
+
+# ----- RUN FOR ALL THE PARAMETERS ----- #
+
+for N, phi_pack, nu_0, J, eta_n in combinations:
+
+    # change the parameters
+    parameters['N'] = N
+    parameters['nu_0'] = nu_0
+    parameters['J'] = J
+    parameters['eta_n'] = eta_n
+    parameters['phi_pack'] = phi_pack
+
+    sim_sphere_fortran.main(parameters)
